87c2bedc283805e894780c9e6b10b03939260f7f
noamdlife15u <joonwoo3816@gmail.com>
2019-08-15 23:58:27 +0900

    blogapp : add user form / update template

--- flask_blog/flaskblog.py
+++ flask_blog/flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'fecde4567bb23ecd02a67c432e096f73'
 
 @app.route('/')
 @app.route('/home')
@@ -10,6 +14,18 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+    
 if __name__ == '__main__':
     app.run(debug=True)
