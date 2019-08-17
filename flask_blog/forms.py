466ce907bc0f7407f61a2680115cac66422eb46b
noamdlife15u <joonwoo3816@gmail.com>
2019-08-17 17:01:41 +0900

    blogapp : update register and login form

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -13,4 +13,4 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('_Login_')
+    submit = SubmitField('Login')
