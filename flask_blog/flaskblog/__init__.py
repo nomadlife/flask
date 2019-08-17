5ac93e6bd2ab02e0fa14a7aee92dfd66e65f21f3
noamdlife15u <joonwoo3816@gmail.com>
2019-08-17 21:38:18 +0900

    blogapp : pw encryption / register function

--- flask_blog/flaskblog/__init__.py
+++ flask_blog/flaskblog/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fecde4567bb23ecd02a67c432e096f73'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt()
 
 from flaskblog import routes
