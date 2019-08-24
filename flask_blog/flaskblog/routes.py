bc0299d49f489e95480aef970c5e95fc6a4e75f3
noamdlife15u <joonwoo3816@gmail.com>
2019-08-24 14:34:58 +0900

    blogapp : add function creating post

--- flask_blog/flaskblog/routes.py
+++ flask_blog/flaskblog/routes.py
@@ -10,7 +10,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
@@ -98,9 +99,13 @@ def account():
     return render_template('account.html', title='Account', image_file = image_file, form=form)
 
 @app.route('/post/new', methods=['GET','POST'])
+@login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
